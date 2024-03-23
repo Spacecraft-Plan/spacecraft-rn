f0489002da94443ea161c024551e6c7c18a0769a
electrolyteJ <hawks.jamesf@gmail.com>
2024-03-24 01:42:15 +0800

    n

--- apps/hotel/index.web1.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import MyReactNativeWebRenderer from '@spacecraft/myrenderer/src/myReactNativeWebRenderer';
-import App from '@spacecraft/myrenderer/src/App';
-
-MyReactNativeWebRenderer.render(<App />, document.getElementById('root'));
--- packages/myrenderer/src/text.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Text extends Component {
-  render() {
-    return <text>{this.props.children}</text>;
-  }
-}
-
-export default Text;
